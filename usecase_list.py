2862f478d18e052932ab2096c04229b2af1b5a6d
hitman452 <khairnarhitesh542@gmail.com>
2025-08-25 00:14:19 +0530

    Add files via upload

---
+++ usecase_list.py
@@ -0,0 +1,10 @@
+to_do = ['python', 'ds', 'deld', 'os', 'oopcg']
+print(to_do,'\n')
+
+to_do.append('java')
+to_do.append('projects')
+to_do.append('assignments')
+print(to_do)
+
+if 'python' in to_do:
+    print("\nDo python on daily basis, that'll help you in ds also" )
